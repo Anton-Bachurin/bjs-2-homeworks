9379d73a98c69bb293cf005a07e7445772127181
Anton Bachurin <ant.bachurin@gmail.com>
2023-06-25 20:46:10 +0400

    Полностью готово второе ДЗ

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -93,14 +93,19 @@ console.log(averageEvenElementsWorker(1, 2, 3, 4, 5, 6, 7, 8, 9));
 console.log(averageEvenElementsWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35));
 
 function makeWork (arrOfArr, func) {
-  let maxWorkerResult = arrOfArr[0];
+  let maxWorkerResult = func(...arrOfArr[0]);
 
   for (let i = 0; i < arrOfArr.length; i++) {
-    if (arrOfArr[i] > maxWorkerResult) {
-    	maxWorkerResult = arrOfArr[i];
+    if (func(...arrOfArr[i]) > maxWorkerResult) {
+    	maxWorkerResult = func(...arrOfArr[i]);
     }
   }
   return maxWorkerResult;
 }
 
 const arr = [[10, 10, 11, 20, 10], [67, 10, 2, 39, 88], [72, 75, 51, 87, 43], [30, 41, 55, 96, 62]];
+
+console.log(makeWork(arr, summElementsWorker)); 
+console.log(makeWork(arr, differenceMaxMinWorker)); 
+console.log(makeWork(arr, differenceEvenOddWorker)); 
+console.log(makeWork(arr, averageEvenElementsWorker));
