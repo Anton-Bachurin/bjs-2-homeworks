f33c1a037b7d55e37087b8b7aefd4fd3f67f0d95
Anton Bachurin <ant.bachurin@gmail.com>
2023-07-26 21:49:49 +0400

    4 задание не работает корректно

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
+let firstStudent = new Student("Artem", "male", 19);
+let secondStudent = new Student("Elena", "female", 22);
+let thirdStudent = new Student("Anna", "female", 18);
+
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if ('marks' in Student) {	
+  this.marks.push(marks);
+  };  
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (!'marks' in Student || Student.marks === []) {
+  	return 0;
+  } else {
+  	let average = Student.marks.reduce((acc, item, index, arr) => acc + item / arr.length, 0);
+  	return average;
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
