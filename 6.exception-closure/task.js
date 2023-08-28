8baa24ea51abd3a2c573ef9452579257a7d0599e
Anton Bachurin <ant.bachurin@gmail.com>
2023-08-28 20:59:54 +0400

    Неполное первое задание

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,20 @@
-﻿
+﻿function parseCount(numberToParse) {
+  if (parseFloat(numberToParse) === NaN) {
+  	const error = new Error("Невалидное значение");
+	throw error;
+  }
+  return parseFloat(numberToParse);
+}
+
+function validateCount(numberToParse) {
+  parseCount(numberToParse);
+  if (parseCount !== NaN) {
+  	return parseCount(numberToParse);
+  }
+
+  try {
+ 	parseCount(numberToParse);
+  } catch (error) {
+    return error;
+  }
+}
